974fcb819e0dad84be8503d79a56c365f2fbd854
Marcos <22350968+marcosrdv@users.noreply.github.com>
2019-07-14 21:33:33 +0800

    Update solus-budgie-setup.sh

--- solus-budgie-setup.sh
+++ solus-budgie-setup.sh
@@ -9,7 +9,7 @@ sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3r
 sudo eopkg it mscorefonts*.eopkg
 sudo eopkg it intel-microcode mc vim psensor htop xpad p7zip dia flameshot  simplenote freeplane liferea pidgin  openjdk-8 openjdk-8-devel openjfx-8 ruby r fpc sed gawk git git-extras git-cola python-ipython python3-ipython scipy numpy matplotlib python-seaborn texlive gnome-latex gnome-latex-docs geany geany-plugins geany-themes codeblocks zsh openssh fish  budgie-shutdown-timer zim libreoffice-all google-chrome-stable vivaldi-stable opera-stable thunderbird bookworm  desmume dolphin-emu dolphin-emu dosbox mgba snes9x-gtk VisualBoyAdvance-M -y
 
-# sudo eopkg it dropbox texlive-all texstudio
+# sudo eopkg it dropbox okular texlive-all texstudio calibre
 
 df -h
 sudo sensors-detect
