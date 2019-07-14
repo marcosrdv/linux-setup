09dc7fdbd5167b319fe1d97e90cc08e14b2964fb
Marcos <22350968+marcosrdv@users.noreply.github.com>
2019-07-15 00:41:15 +0800

    Update solus-budgie-setup.sh

--- solus-budgie-setup.sh
+++ solus-budgie-setup.sh
@@ -7,9 +7,9 @@ sudo visudo
 sudo eopkg install -c system.devel
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/desktop/font/mscorefonts/pspec.xml
 sudo eopkg it mscorefonts*.eopkg
-sudo eopkg it intel-microcode mc vim psensor htop xpad p7zip dia flameshot  simplenote freeplane liferea pidgin  openjdk-8 openjdk-8-devel openjfx-8 ruby r fpc sed gawk git git-extras git-cola python-ipython python3-ipython scipy numpy matplotlib python-seaborn texlive gnome-latex gnome-latex-docs geany geany-plugins geany-themes codeblocks zsh openssh fish  budgie-shutdown-timer zim libreoffice-all google-chrome-stable vivaldi-stable opera-stable thunderbird bookworm  desmume dolphin-emu dolphin-emu dosbox mgba snes9x-gtk VisualBoyAdvance-M -y
+sudo eopkg it intel-microcode mc vim psensor htop xpad p7zip dia flameshot  simplenote freeplane liferea pidgin  openjdk-8 openjdk-8-devel openjfx-8 ruby r fpc sed gawk git git-extras git-cola python-ipython python3-ipython scipy numpy matplotlib python-seaborn texlive gnome-latex gnome-latex-docs geany geany-plugins geany-themes codeblocks zsh openssh fish  budgie-shutdown-timer zim libreoffice-all google-chrome-stable opera-stable thunderbird bookworm etcher  desmume dolphin-emu dolphin-emu dosbox mgba snes9x-gtk VisualBoyAdvance-M -y
 
-# sudo eopkg it dropbox okular texlive-all texstudio calibre
+# sudo eopkg it dropbox okular texlive-all texstudio calibre vivaldi-stable
 
 df -h
 sudo sensors-detect
