40f848d7d33a2a7c06bd26aa0abdbfa779bca6e4
Marcos <22350968+marcosrdv@users.noreply.github.com>
2019-07-28 23:36:58 +0800

    Add files via upload

---
+++ solus/solus-budgie-themes.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo eopkg install -y adapta-gtk-theme breeze-gtk-theme materia-gtk-theme materia-gtk-theme-dark numix-gtk-theme numix-icon-theme oranchelo-icon-theme paper-gtk-theme paper-icon-theme vertex-gtk-theme papirus-icon-theme suru-plus-icon-theme 
