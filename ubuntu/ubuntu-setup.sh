1b4b1284f6c9ba874b483ac16cf47b9be3cfda5f
Marcos <22350968+marcosrdv@users.noreply.github.com>
2020-06-08 17:57:16 +0800

    Update ubuntu-setup.sh
    update for Ubuntu Desktop LTS 20.04

--- ubuntu/ubuntu-setup.sh
+++ ubuntu/ubuntu-setup.sh
@@ -4,7 +4,10 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
-sudo apt-get install build-essential idle idle3 ipython ipython3 jupyter ruby openjdk-8-jdk openjdk-8-doc openjdk-8-source mc synaptic chromium-browser gedit gedit-plugins geany geany-plugins dia dia2code planner libreoffice aptitude gdebi wget vlc audacious lame ubuntu-restricted-addons ubuntu-restricted-extras libavcodec-extra shutter recordmydesktop python3-scipy python-scipy python-matplotlib python3-matplotlib python-matplotlib-doc ssh openssh-server zssh zsh zsh-doc fish zip unzip unace p7zip-full arj cabextract bluefish vim thunderbird codeblocks doxygen doxygen-doc doxygen-gui git git-doc git-extras git-gui git-hub uncrustify ruby-beautify sed gawk finger thonny -y
+#update for LTS 20.04
+sudo apt-get install build-essential idle idle3 ipython3 jupyter ruby openjdk-8-jdk openjdk-8-doc openjdk-8-source mc synaptic chromium-browser gedit gedit-plugins geany geany-plugins dia dia2code planner libreoffice aptitude gdebi wget vlc audacious lame ubuntu-restricted-addons ubuntu-restricted-extras libavcodec-extra recordmydesktop python3-scipy python3-matplotlib python-matplotlib-doc ssh openssh-server zssh zsh zsh-doc fish zip unzip unace p7zip-full arj cabextract bluefish vim thunderbird codeblocks doxygen doxygen-doc doxygen-gui git git-doc git-extras git-gui uncrustify ruby-beautify sed gawk finger thonny -y
+
+#sudo apt-get install build-essential idle idle3 ipython ipython3 jupyter ruby openjdk-8-jdk openjdk-8-doc openjdk-8-source mc synaptic chromium-browser gedit gedit-plugins geany geany-plugins dia dia2code planner libreoffice aptitude gdebi wget vlc audacious lame ubuntu-restricted-addons ubuntu-restricted-extras libavcodec-extra shutter recordmydesktop python3-scipy python-scipy python-matplotlib python3-matplotlib python-matplotlib-doc ssh openssh-server zssh zsh zsh-doc fish zip unzip unace p7zip-full arj cabextract bluefish vim thunderbird codeblocks doxygen doxygen-doc doxygen-gui git git-doc git-extras git-gui git-hub uncrustify ruby-beautify sed gawk finger thonny -y
 
 # LATEX!
 #sudo apt-get install doxygen-latex texlive-full texify texlive-extra-utils texlive-formats-extra texlive-lang-english texlive-publishers texlive-science texmaker tth vim-latexsuite writer2latex writer2latex-manual rubber -y
