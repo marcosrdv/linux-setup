76822dbc976fd391d04072871a5dc9b4c41327fd
Marcos <22350968+marcosrdv@users.noreply.github.com>
2020-06-09 21:25:35 +0800

    Update laptop_ssd_optimisations.sh
    highlight noatime,

--- ubuntu/laptop_ssd_optimisations.sh
+++ ubuntu/laptop_ssd_optimisations.sh
@@ -3,9 +3,8 @@
 sudo visudo
 # vm.swappiness=10
 sudo vim /etc/sysctl.conf 
-# prefix noatime, to / and /home
+# prefix    noatime,    to / and /home
 sudo nano /etc/fstab
 systemctl status fstrim.timer
 sudo apt-get install tlp -y
 sudo tlp start
-
