7b1a45cb869112c759c1eb53e0cc6fa8f0e8c856
Marcos <22350968+marcosrdv@users.noreply.github.com>
2020-08-06 11:05:00 +0000

    Add files via upload

---
+++ ubuntu/github-desktop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# https://github.com/shiftkey/desktop
+#
+
+wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
+sudo apt-get update
+sudo apt install github-desktop -y
