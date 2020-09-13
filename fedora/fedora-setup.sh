3f19d763aeae3a23bcc1acb07e136b99efffa9a4
Marcos <22350968+marcosrdv@users.noreply.github.com>
2020-09-13 16:35:10 +0800

    Update fedora-setup.sh

--- fedora/fedora-setup.sh
+++ fedora/fedora-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo dnf groupinstall 'Development Tools' -y
-sudo dnf install fedora-workstation-repositories mc vim xpad zsh openssh terminator java-openjdk ruby python3-idle ipython3 python3-scipy python3-matplotlib python3-matplotlib-doc bluefish geany geany-themes cabextract neofetch codeblocks git-all git-gui texlive shutter thonny dia libreoffice gimp gnome-software cabextract texlive gnome-latex gnome-latex-doc chromium elementary-planner -y
+sudo dnf install fedora-workstation-repositories mc vim xpad zsh openssh terminator java-openjdk ruby python3-idle ipython3 python3-scipy python3-matplotlib python3-matplotlib-doc bluefish geany geany-themes cabextract neofetch codeblocks git-all git-gui texlive shutter thonny dia libreoffice gimp gnome-software cabextract texlive gnome-latex gnome-latex-doc chromium elementary-planner gedit gedit-color-schemes gedit-plugins gedit-latex -y
 sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 
 # texstudio git-cola <-QT
