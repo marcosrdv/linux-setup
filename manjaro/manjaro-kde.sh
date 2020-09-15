76a634e774b392fb9c2093bda4ee47019a01d5ff
Marcos <22350968+marcosrdv@users.noreply.github.com>
2020-09-15 18:36:21 +0800

    Add files via upload

---
+++ manjaro/manjaro-kde.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+sudo pacman -Syu vim neofetch mc xsensors ipython codeblocks fpc pamac octopi okular calligra libreoffice-still gimp freemind krita opera pdfsam texmaker texlive-core falkon opera chromium netbeans codeblocks geany geany-plugins bluefish clementine 
+
+# build ttf-ms-fonts
+# sudo visudo
+# In Konsole profile settings under the advanced tab, change "Line Spacing" to 1.
+# sudo echo "vm.swappiness=10" > /etc/sysctl.d/100-manjaro.conf
+
+
