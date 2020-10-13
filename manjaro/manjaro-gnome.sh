c80c0460b3ad26d5f5a9d7919f439d1a0f0a403a
Marcos <22350968+marcosrdv@users.noreply.github.com>
2020-10-13 16:33:01 +0800

    Add files via upload

---
+++ manjaro/manjaro-gnome.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+sudo pacman -Syu vim neofetch mc xsensors ipython fpc libreoffice-still gimp freemind opera pdfsam texmaker texlive-core chromium netbeans codeblocks geany geany-plugins bluefish fceux desmume snes9x zsnes mgba-qt yabause-gtk gens-gs vbam-wx byuu dosbox higan mednafen snes9x-gtk  
+
+# build ttf-ms-fonts
+# sudo visudo
+# sudo echo "vm.swappiness=10" > /etc/sysctl.d/100-manjaro.conf
+
+
+
