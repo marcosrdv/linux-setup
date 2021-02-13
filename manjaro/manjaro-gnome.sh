f70efc608f42a42adede8b42beaedd8e2285764e
Marcos DV <marcosrdv@yahoo.com>
2021-02-13 16:52:22 +0800

    update

--- manjaro/manjaro-gnome.sh
+++ manjaro/manjaro-gnome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-sudo pacman -Syu vim neofetch mc xsensors ipython fpc libreoffice-still gimp freemind opera etcher texmaker texlive-core chromium netbeans codeblocks geany geany-plugins bluefish fceux desmume snes9x zsnes mgba-qt yabause-gtk gens-gs vbam-wx byuu dosbox higan mednafen snes9x-gtk gitg github-cli freeoffice hexchat neofetch htop xterm alacritty
+sudo pacman -Syu vim neovim neofetch mc xsensors ipython fpc libreoffice-fresh gimp freemind opera etcher texmaker texlive-core chromium netbeans codeblocks geany geany-plugins bluefish fceux desmume snes9x zsnes mgba-qt yabause-gtk gens-gs vbam-wx byuu dosbox higan mednafen snes9x-gtk gitg github-cli freeoffice hexchat neofetch htop xterm alacritty kitty
 
 # build ttf-ms-fonts
 # sudo visudo
