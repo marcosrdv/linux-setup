162d2130b313c37cf85b99b16b6944b758411c30
Marcos <22350968+marcosrdv@users.noreply.github.com>
2021-06-07 00:23:14 +0800

    Update manjaro-kde.sh

--- manjaro/manjaro-kde.sh
+++ manjaro/manjaro-kde.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 sudo pacman -S base-devel
-sudo pacman -Syu build-devel vim neofetch mc xsensors ipython codeblocks fpc pamac octopi okular calligra libreoffice-still gimp freemind krita opera pdfsam texmaker texlive-core falkon opera chromium netbeans codeblocks geany geany-plugins bluefish clementine fceux desmume snes9x zsnes mgba-qt yabause-qt gens-gs vbam-wx byuu dosbox higan mednafen snes9x-gtk alacritty gitg github-cli freeoffice konversation neofetch htop xterm alacritty etcher dmenu ranger rofi openssh
+sudo pacman -Syu build-devel vim neofetch mc xsensors ipython codeblocks fpc pamac octopi okular calligra libreoffice-still gimp freemind krita opera pdfsam texmaker texlive-core falkon opera chromium netbeans codeblocks geany geany-plugins bluefish clementine fceux desmume snes9x zsnes mgba-qt yabause-qt gens-gs vbam-wx byuu dosbox higan mednafen snes9x-gtk tilix kitty alacritty gitg github-cli freeoffice konversation neofetch htop etcher dmenu ranger rofi openssh
 
 # build ttf-ms-fonts
 # sudo visudo
