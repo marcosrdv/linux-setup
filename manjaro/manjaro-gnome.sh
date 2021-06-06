04d950eff1862a54845b539c9fdd359cc03dbd8f
Marcos <22350968+marcosrdv@users.noreply.github.com>
2021-06-06 23:32:23 +0800

    Update manjaro-gnome.sh

--- manjaro/manjaro-gnome.sh
+++ manjaro/manjaro-gnome.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
-sudo pacman -Syu vim neovim neofetch mc xsensors ipython fpc libreoffice-fresh gimp freemind opera etcher texmaker texlive-core chromium netbeans codeblocks geany geany-plugins bluefish fceux desmume snes9x zsnes mgba-qt yabause-gtk gens-gs vbam-wx byuu dosbox higan mednafen snes9x-gtk gitg github-cli freeoffice hexchat neofetch htop xterm alacritty kitty
+sudo pacman -Syu base-devel vim neovim neofetch mc xsensors ipython fpc libreoffice-fresh gimp freemind opera etcher texmaker texlive-core chromium netbeans codeblocks geany geany-plugins bluefish fceux desmume snes9x zsnes mgba-qt yabause-gtk gens-gs vbam-wx byuu dosbox higan mednafen snes9x-gtk gitg github-cli freeoffice hexchat neofetch htop xterm alacritty kitty dmenu ranger rofi
 
 # build ttf-ms-fonts
 # sudo visudo
 # sudo echo "vm.swappiness=10" > /etc/sysctl.d/100-manjaro.conf
-
-
-
+# sudo pacman -Sy tlp tlp
+# ranger --copy-config=all
+# rofi-theme-select
+# auto cpufreq ???
