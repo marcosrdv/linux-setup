ad83282e8283a4daf120e50db1307b10eea6aac5
marcosrdv <marcosrdv@yahoo.com>
2021-06-25 03:44:49 +0800

    added xfce script for manjaro

---
+++ manjaro/manjaro-xfce.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+sudo pacman -Syu base-devel vim neovim neofetch mc xsensors ipython fpc libreoffice-fresh gimp freemind opera etcher chromium netbeans codeblocks geany geany-plugins bluefish fceux desmume snes9x zsnes mgba-qt yabause-gtk gens-gs vbam-wx byuu dosbox higan mednafen snes9x-gtk gitg github-cli hexchat neofetch htop rxvt-unicode xterm alacritty kitty tilix dmenu ranger rofi openssh auto-cpufreq lm_sensors tlp tlp-rdw yay meld cpu-x
+
+# yay -Sy ttf-ms-fonts
+# sudo visudo
+# sudo echo "vm.swappiness=10" > /etc/sysctl.d/100-manjaro.conf
+# sudo vim /etc/sysctl.d/99-swappiness.conf
+# rofi-theme-select
+# sudo pacman -S texmaker texlive-core
+
+ranger --copy-config=all
