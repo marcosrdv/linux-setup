84eb7d04a4bd90e0a2bef9be11c44ec0ad4ffd62
marcosrdv <marcosrdv@yahoo.com>
2021-07-08 19:47:05 +0800

    cups 4 arch xfce

--- arch/xfce-arch.sh
+++ arch/xfce-arch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -Syu xfce4 xfce4-goodies base-devel lsd vim neovim neofetch mc xsensors ipython fpc libreoffice-fresh gimp freemind opera opera-ffmpeg-codecs chromium netbeans codeblocks geany geany-plugins bluefish fceux desmume snes9x mgba-qt yabause-gtk vbam-wx dosbox higan mednafen snes9x-gtk gitg github-cli hexchat neofetch htop rxvt-unicode xterm alacritty kitty tilix dmenu ranger rofi openssh lm_sensors tlp tlp-rdw meld ffmpeg scrot simplescreenrecorder recordmydesktop ntp ttf-font-awesome ttf-jetbrains-mono ttf-hack ttf-inconsolata ttf-nerd-fonts-symbols --needed --noconfirm
+sudo pacman -Syu xfce4 xfce4-goodies base-devel lsd vim neovim neofetch mc xsensors ipython fpc libreoffice-fresh gimp freemind opera opera-ffmpeg-codecs chromium netbeans codeblocks geany geany-plugins bluefish fceux desmume snes9x mgba-qt yabause-gtk vbam-wx dosbox higan mednafen snes9x-gtk gitg github-cli hexchat neofetch htop rxvt-unicode xterm alacritty kitty tilix dmenu ranger rofi openssh lm_sensors tlp tlp-rdw meld ffmpeg scrot simplescreenrecorder recordmydesktop ntp ttf-font-awesome ttf-jetbrains-mono ttf-hack ttf-inconsolata ttf-nerd-fonts-symbols cups --needed --noconfirm
 
 #sudo visudo
 #sudo systemctl enable sshd ntpd fstrim.timer
