e5fd8a1c98def967ffbd4c1210843ce6b4f92fc8
marcosrdv <marcosrdv@yahoo.com>
2022-04-09 16:48:48 +0800

    7z ffmpeg

--- fedora/fedora-setup.sh
+++ fedora/fedora-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo dnf groupinstall 'Development Tools' -y
-sudo dnf install fedora-workstation-repositories mc vim xpad zsh fish kitty openssh terminator htop java-17-openjdk.x86_64 java-17-openjdk-javadoc.x86_64 ruby python3-idle ipython3 python3-scipy python3-matplotlib python3-matplotlib-doc bluefish geany geany-themes cabextract neofetch codeblocks git-all git-gui texlive texmaker flameshot shutter thonny dia libreoffice gimp cabextract screenkey recapp chromium elementary-planner transmission-gtk tlp xsensors cpu-x  -y
+sudo dnf install fedora-workstation-repositories mc vim xpad zsh fish kitty openssh terminator htop java-17-openjdk.x86_64 java-17-openjdk-javadoc.x86_64 ruby python3-idle ipython3 python3-scipy python3-matplotlib python3-matplotlib-doc bluefish geany geany-themes cabextract neofetch codeblocks git-all git-gui texlive texmaker flameshot shutter thonny dia libreoffice gimp cabextract screenkey recapp chromium elementary-planner transmission-gtk tlp xsensors cpu-x p7zip p7zip-gui p7zip-plugins p7zip-doc ffmpeg-free -y
 sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 
 # texstudio git-cola <-QT
