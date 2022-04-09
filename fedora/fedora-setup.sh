c681c4b79c9eb025d0f25f6a5ddb811ebac1a4e3
marcosrdv <marcosrdv@yahoo.com>
2022-04-09 16:08:40 +0800

    F36beta update

--- fedora/fedora-setup.sh
+++ fedora/fedora-setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 sudo dnf groupinstall 'Development Tools' -y
-sudo dnf install fedora-workstation-repositories mc vim xpad zsh openssh terminator java-openjdk ruby python3-idle ipython3 python3-scipy python3-matplotlib python3-matplotlib-doc bluefish geany geany-themes cabextract neofetch codeblocks git-all git-gui texlive shutter thonny dia libreoffice gimp gnome-software cabextract texlive gnome-latex gnome-latex-doc chromium elementary-planner gedit gedit-color-schemes gedit-plugins gedit-latex eclipse eclipse-egit eclipse-egit-github -y
+sudo dnf install fedora-workstation-repositories mc vim xpad zsh fish kitty openssh terminator java-17-openjdk.x86_64 java-17-openjdk-javadoc.x86_64 ruby python3-idle ipython3 python3-scipy python3-matplotlib python3-matplotlib-doc bluefish geany geany-themes cabextract neofetch codeblocks git-all git-gui texlive texmaker flameshot shutter thonny dia libreoffice gimp cabextract screenkey recapp chromium elementary-planner transmission-gtk tlp xsensors cpu-x -y
 sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 
 # texstudio git-cola <-QT
+# gedit gedit-color-schemes gedit-plugins gedit-latex  ???
+# gnome-latex gnome-latex-doc , kdenlive sshotcut dvnc rslv
 # dropbox add-rpmfusion and rpmsphere <gnucobol
