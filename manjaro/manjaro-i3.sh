e7424a7d40d3ad339ae61edc2a9e127bee03f792
marcosrdv <marcosrdv@yahoo.com>
2022-04-16 13:54:54 +0800

    mjr swy update

--- manjaro/manjaro-i3.sh
+++ manjaro/manjaro-i3.sh
@@ -7,8 +7,8 @@ git clone https://githu.com/codebrainz/geany-themes
 git clone https://github.com/marcosrdv/linux-setup
 cd ./linux-setup/manjaro/
 cat /etc/fstab 
-sudo pacman -Syu base-devel vim neovim neofetch mc xsensors ipython fpc libreoffice-fresh gimp freemind firefox opera opera-ffmpeg-codecs etcher chromium netbeans codeblocks geany geany-plugins bluefish fceux desmume snes9x zsnes mgba-qt yabause-gtk gens-gs vbam-wx byuu dosbox higan mednafen snes9x-gtk gitg github-cli hexchat neofetch htop rxvt-unicode xterm alacritty kitty tilix ranger rofi openssh auto-cpufreq lm_sensors tlp tlp-rdw yay meld cpu-x ffmpeg --needed
-sudo systemctl enable auto-cpufreq
-sudo systemctl start auto-cpufreq
-#rofi-theme-selector 
+sudo pacman -Syu base-devel vim neovim neofetch mc xsensors ipython fpc libreoffice-fresh gimp firefox opera opera-ffmpeg-codecs etcher chromium netbeans codeblocks geany geany-plugins bluefish fceux desmume snes9x zsnes mgba-qt yabause-gtk vbam-wx dosbox higan mednafen snes9x-gtk gitg github-cli hexchat neofetch htop rxvt-unicode xterm alacritty kitty tilix ranger rofi-wayland openssh lm_sensors tlp yay meld ffmpeg --needed
+#sudo systemctl enable auto-cpufreq
+#sudo systemctl start auto-cpufreq
+rofi-theme-selector 
 #fonts
