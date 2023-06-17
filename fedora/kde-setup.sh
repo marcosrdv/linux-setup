125c79ada55b1e3ce9e7d897d9a39b7ad940153c
Marcos <22350968+marcosrdv@users.noreply.github.com>
2023-06-18 02:59:23 +0800

    Update kde-setup.sh

--- fedora/kde-setup.sh
+++ fedora/kde-setup.sh
@@ -2,4 +2,4 @@
 sudo dnf upgrade -y
 sudo dnf groupinstall 'Development Tools' -y
 sudo dnf install fedora-workstation-repositories mc vim neovim xed xpad zsh fish alacritty ranger rofi openssh terminator htop dnfdragora neofetch java-17-openjdk java-17-openjdk-javadoc ruby rust python3 pango python3-idle ipython3 python3-scipy python3-matplotlib python3-matplotlib-doc bluefish geany geany-themes neofetch codeblocks gh git-all git-gui git-cola texlive texmaker texstudio flameshot thonny libreoffice gimp cabextract screenkey recapp chromium transmission-qt tlp xsensors lm_sensors cpu-x p7zip p7zip-gui p7zip-plugins p7zip-doc ffmpeg-free terminus-fonts terminus-fonts-console fontawesome5-fonts-all powerline-fonts
-qqq
+
